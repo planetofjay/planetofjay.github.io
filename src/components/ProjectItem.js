35cfad3602ead4a75b08a4298e77d381320d460d
PlanetofJay <jae44dv@gmail.com>
2023-04-28 19:22:16 -0400

    fix bugs and styles

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function ProjectItem({ image, name }) {
+export default function ProjectItem({ image, name, id }) {
+  const navigate = useNavigate();
   return (
-    <div className='projectItem'>
+    <div 
+      className='projectItem'
+      onClick={() => {
+        navigate('/project/' + id);
+      }}
+    >
       <div
         style={{ backgroundImage: `url(${image})` }}
         className='bgImage'
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ProjectItem from '../components/ProjectItem';
 import { ProjectList } from '../helpers/ProjectList';
-
 import '../styles/Projects.css';
 
 export default function Projects() {
   return (
     <div className='projects'>
-      <h1>My Personal Projects</h1>
-      <div className='projectList'></div>
-        {ProjectList.map((project) => {
-          return <ProjectItem name={project.name} image={project.image} />;
+      <h1 className='projectTitle'>My Personal Projects</h1>
+      <div className='projectList'>
+        {ProjectList.map((project, index) => {
+          return (
+            <ProjectItem 
+              id={index} 
+              name={project.name} 
+              image={project.image} 
+            />
+          );
         })}
+      </div>
     </div> 
   );
 }
