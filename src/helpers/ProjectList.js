58465a14b24a650290f494946f10204387b71f97
PlanetofJay <jae44dv@gmail.com>
2023-04-28 18:06:44 -0400

    create helpers and map

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,33 @@
+import Proj1 from '../assets/proj1.png';
+import Proj2 from '../assets/proj2.png';
+import Proj3 from '../assets/proj3.png';
+import Proj4 from '../assets/proj4.png';
+import Proj5 from '../assets/proj5.png';
+import Proj6 from '../assets/proj6.png';
+
+export const ProjectList = [
+  {
+    name: 'thejaejinlee Magazine Website',
+    image: Proj1,
+  },
+  {
+    name: 'Weather Application',
+    image: Proj2,
+  },
+  {
+    name: 'Social Media Website',
+    image: Proj3,
+  },
+  {
+    name: 'WindowAdvisor Mobile Application',
+    image: Proj4,
+  },
+  {
+    name: 'Tetris Application',
+    image: Proj5,
+  },
+  {
+    name: 'Interactive Rating Component',
+    image: Proj6,
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProjectItem from '../components/ProjectItem';
-import Proj1 from '../assets/proj1.png';
-import Proj2 from '../assets/proj2.png';
+import { ProjectList } from '../helpers/ProjectList';
 
 import '../styles/Projects.css';
 
@@ -10,8 +9,9 @@ export default function Projects() {
     <div className='projects'>
       <h1>My Personal Projects</h1>
       <div className='projectList'></div>
-        <ProjectItem name='Magazine Website' image={Proj1}/>
-        <ProjectItem name='Weather Application' image={Proj2}/>
+        {ProjectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
     </div> 
   );
 }
