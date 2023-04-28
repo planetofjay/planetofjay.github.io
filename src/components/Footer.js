96d55e62866d45baa3165261d83cc64a062429e5
PlanetofJay <jae44dv@gmail.com>
2023-04-28 01:48:32 -0400

    update home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import GithubIcon from '@material-ui/icons/GitHub';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import '../styles/Footer.css';
@@ -10,7 +9,6 @@ function Footer() {
     <div className='footer'>
       <div className='socialMedia'> 
         <InstagramIcon />
-        <GithubIcon />
         <FacebookIcon />
         <LinkedInIcon />
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import GithubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import '../styles/Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='about'>
+        <h2>Hi, My Name is Jaejin</h2>
+        <div className='prompt'>
+          <p>
+            A frontend developer with a passion for learning and creating.
+          </p>
+          <GithubIcon />
+          <EmailIcon />
+          <LinkedInIcon />
+        </div>
+      </div>
+      <div className='skills'>
+        <h1>Skills</h1>
+        <ol className='list'>
+
+          <li className='item'>
+            <h2>Front-End</h2>
+            <span>
+              ReactJS, Redux, HTML, CSS, Sass, React Native
+            </span>
+          </li>          
+
+          <li className='item'>
+            <h2>Back-End</h2>
+            <span>
+              MySQL, Firebase, Axios
+            </span>
+          </li>     
+
+          <li className='item'>
+            <h2>Languages</h2>
+            <span>JavaScript, TypeScript</span>
+          </li>
+
+        </ol>
+      </div>
+    </div>
   )
 }
 
