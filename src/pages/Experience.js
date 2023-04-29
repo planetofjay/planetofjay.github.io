c7a1754c3399dbd128b83eaa3bb579d788a69def
PlanetofJay <jae44dv@gmail.com>
2023-04-28 20:39:13 -0400

    update

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -8,55 +8,29 @@ export default function Experience() {
   return (
     <div className='experience'>
       <VerticalTimeline lineColor='#3e497a'>
-        <VerticalTimelineElement 
+        <VerticalTimelineElement
           className='vertical-timeline-element--education'
-          date='2009 - 2012'
-          iconStyle={{ background: '#3e497a', color: '#fff'}}
-          icon={<SchoolIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'> 
-            Sangmyung High School, Seoul, South Korea
-          </h3>
-          <p>High School Diploma</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement 
-          className='vertical-timeline-element--education'
-          date='2013 - 2015'
-          iconStyle={{ background: '#3e497a', color: '#fff'}}
-          icon={<SchoolIcon />}
-        >
-          <h3 className='vertical-timeline-element-title'> 
-            Induk University, Seoul, South Korea
-          </h3>
-          <h4 className='vertical-timeline-element-subtitle'>
-            Incomplete
-          </h4>
-          <p>Urban Environment Design</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement 
-          className='vertical-timeline-element--education'
-          date='2017 - 2020'
-          iconStyle={{ background: '#e9d35b', color: '#fff'}}
+          date='2023 - Present'
+          iconStyle={{ background: '#e9d35b', color: '#fff' }}
           icon={<WorkIcon />}
         >
-          <h3 className='vertical-timeline-element-title'> 
-            Photographer - Freelance
+          <h3 className='vertical-timeline-element-title'>
+            Software Developer Intern - GAO Tek
           </h3>
           <h4 className='vertical-timeline-element-subtitle'>
-            Dublin, Ireland / Paris, France
+            Toronto, ON - remote
           </h4>
-          <p>Had exhibitions in Seoul and Belgium, published own photography book using Indesign.</p>
+          <p>assigned with tasks and assignments that focus primarily on
+            developing new skills and gaining a deeper understanding.</p>
         </VerticalTimelineElement>
 
-        <VerticalTimelineElement 
+        <VerticalTimelineElement
           className='vertical-timeline-element--education'
           date='2021 - 2023'
-          iconStyle={{ background: '#3e497a', color: '#fff'}}
+          iconStyle={{ background: '#3e497a', color: '#fff' }}
           icon={<SchoolIcon />}
         >
-          <h3 className='vertical-timeline-element-title'> 
+          <h3 className='vertical-timeline-element-title'>
             Fanshawe College, London, Ontario
           </h3>
           <h4 className='vertical-timeline-element-subtitle'>
@@ -65,20 +39,46 @@ export default function Experience() {
           <p>Web Development and Internet Applications</p>
         </VerticalTimelineElement>
 
-        <VerticalTimelineElement 
+        <VerticalTimelineElement
           className='vertical-timeline-element--education'
-          date='2023 - Present'
-          iconStyle={{ background: '#e9d35b', color: '#fff'}}
+          date='2017 - 2020'
+          iconStyle={{ background: '#e9d35b', color: '#fff' }}
           icon={<WorkIcon />}
         >
-          <h3 className='vertical-timeline-element-title'> 
-            Software Developer Intern - GAO Tek
+          <h3 className='vertical-timeline-element-title'>
+            Photographer - Freelance
           </h3>
           <h4 className='vertical-timeline-element-subtitle'>
-            Toronto, ON - remote
+            Dublin, Ireland / Paris, France
           </h4>
-          <p>assigned with tasks and assignments that focus primarily on
-developing new skills and gaining a deeper understanding.</p>
+          <p>Had exhibitions in Seoul and Belgium, published own photography book using Indesign.</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className='vertical-timeline-element--education'
+          date='2013 - 2015'
+          iconStyle={{ background: '#3e497a', color: '#fff' }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>
+            Induk University, Seoul, South Korea
+          </h3>
+          <h4 className='vertical-timeline-element-subtitle'>
+            Incomplete
+          </h4>
+          <p>Urban Environment Design</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className='vertical-timeline-element--education'
+          date='2009 - 2012'
+          iconStyle={{ background: '#3e497a', color: '#fff' }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>
+            Sangmyung High School, Seoul, South Korea
+          </h3>
+          <p>High School Diploma</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
