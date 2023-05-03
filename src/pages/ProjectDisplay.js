fe18b76757f71fe10bd0f8b42ecca33566e4c4f6
PlanetofJay <jae44dv@gmail.com>
2023-05-03 01:41:04 -0400

    add url

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { ProjectList } from '../helpers/ProjectList';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import URLIcon from '@material-ui/icons/Link';
 import '../styles/ProjectDisplay.css';
 
 export default function ProjectDisplay() {
@@ -11,13 +12,18 @@ export default function ProjectDisplay() {
   return (
     <div className='project'>
       <h1> {project.name} </h1>
-      <img src={project.image} alt='projects' className='project-image'/>
+      <img src={project.image} alt='projects' />
       <p>
         <b>Skills:</b> {project.skills} 
       </p>
-      <a href={project.url} target='_blank' rel="noreferrer">
-        <GitHubIcon />
-      </a>
+      <div className='project-icon'>
+        <a href={project.repo} target='_blank' rel='noreferrer'>
+          <GitHubIcon />
+        </a>
+        <a href={project.url} target='_blank' rel='noreferrer'>
+          <URLIcon />
+        </a>
+      </div>
     </div>
   );
 }
