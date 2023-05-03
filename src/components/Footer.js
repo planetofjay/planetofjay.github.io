1e0286c5087e71ef7446cbf1b6867af676d3a203
PlanetofJay <jae44dv@gmail.com>
2023-05-03 04:27:11 -0400

    return

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,31 +3,22 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import '../styles/Footer.css';
-import '../styles/Scroll.css';
-import Scroll from './Scroll';
 
 export default function Footer() {
   return (
-    <>
-      <div className='page-scroll'>
-        <Scroll />
+    <div className='footer'>
+      <div className='socialMedia'>
+        <a href='https://instagram.com/ofjaylee/' target='_blank' rel="noreferrer">
+          <InstagramIcon />
+        </a>
+        <a href='https://facebook.com/jae4f/' target='_blank' rel="noreferrer">
+          <FacebookIcon />
+        </a>
+        <a href='https://linkedin.com/in/planetofjay/' target='_blank' rel="noreferrer">
+          <LinkedInIcon />
+        </a>
       </div>
-      <div className='footer'>
-
-
-        <div className='socialMedia'>
-          <a href='https://instagram.com/ofjaylee/' target='_blank' rel="noreferrer">
-            <InstagramIcon />
-          </a>
-          <a href='https://facebook.com/jae4f/' target='_blank' rel="noreferrer">
-            <FacebookIcon />
-          </a>
-          <a href='https://linkedin.com/in/planetofjay/' target='_blank' rel="noreferrer">
-            <LinkedInIcon />
-          </a>
-        </div>
-        <p> &copy;2023 Jaejin Lee. All Rights Reserved.</p>
-      </div>
-    </>
+      <p> &copy;2023 Jaejin Lee. All Rights Reserved.</p>
+    </div>
   );
 }
