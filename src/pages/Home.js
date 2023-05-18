ace1c00910ab731ef3d427d09cddd64d6e5006d6
PlanetofJay <jae44dv@gmail.com>
2023-05-17 22:42:01 -0400

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,16 @@ import '../styles/Home.css';
 import homeVideo from './../video/page.mp4';
 
 export default function Home() {
-  
+
   return (
     <div className='home'>
       <div className='about'>
-      <video src={homeVideo} autoPlay loop muted />
+        <video src={homeVideo} autoPlay loop muted />
 
-        <h2>Hi, My Name is Jaejin</h2>
+        <h2>Hi, My Name is Jaejin L.</h2>
         <div className='prompt'>
           <p>
-            a Web Developer with a passion for learning and creating.
+            a Web Developer with a passion for learning, creating, solving, and developing.
           </p>
           <a href='https://github.com/PlanetofJay' target='_blank' rel="noreferrer">
             <GithubIcon />
@@ -28,26 +28,25 @@ export default function Home() {
       <div className='skills'>
         <h1>Skills</h1>
         <ol className='list'>
+          <li className='item'>
+            <h2>Languages</h2>
+            <span>JavaScript</span>
+          </li>
 
           <li className='item'>
             <h2>Front-End</h2>
-            <span>
-              React, Redux, HTML, CSS
-            </span>
-          </li>          
+            <span>React, Redux, React Native, HTML, CSS</span>
+          </li>
 
           <li className='item'>
             <h2>Back-End</h2>
-            <span>
-              Firebase, Axios, MySQL
-            </span>
-          </li>     
-
-          <li className='item'>
-            <h2>Languages</h2>
-            <span>JavaScript, TypeScript</span>
+            <span>MySQL</span>
           </li>
 
+          <li className='item'>
+            <h2>Design-Tool</h2>
+            <span>Adobe XD, Adobe Illustrator, WordPress</span>
+          </li>
         </ol>
       </div>
     </div>
